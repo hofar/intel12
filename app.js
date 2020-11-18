fb9b4b8c0543002809e14c80c7286cdb8553053c
hofar <hofar.ebook@gmail.com>
2020-11-18 18:18:25 +0700

    updated

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
+const express = require("express");
 var http = require("http");
 var url = require('url');
 var qString = require('querystring');
-var routes = require("routes")();
+// var routes = require("routes")();
 var view = require('swig');
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: '104.161.66.10',
     port: 3306,
-    database: 'nodejs',
-    user: 'root',
-    password: '',
+    database: 'hofarism_node',
+    user: 'hofarism_root',
+    password: 'Vu4quis+-',
 });
 
-routes.addRoute('/', function (req, res) {
+const app_url = "/nodejs/api";
+const app = express();
+const router = express.Router();
+
+router.get('/tes', function (req, res) {
+    console.log('tes');
+    res.end('tes page');
+});
+
+router.get('/', function (req, res) {
     connection.query("select * from mahasiswa", function (err, rows, field) {
         if (err) {
-            throw err;
+            if (err) throw err;
         }
         // rows.forEach(function (item) {
         //     console.log(item.no_induk + " ==> " + item.nama);
@@ -25,7 +35,8 @@ routes.addRoute('/', function (req, res) {
 
         var html = view.compileFile('./template/index.html')({
             title: "Data Mahasiswa",
-            data: rows
+            data: rows,
+            app_url: app_url
         });
 
         res.writeHead(200, {"Content-Type": "text/html"});
@@ -33,7 +44,7 @@ routes.addRoute('/', function (req, res) {
     });
 });
 
-routes.addRoute('/insert', function (req, res) {
+router.get('/insert', function (req, res) {
     if (req.method.toUpperCase() == "POST") {
         var data_post = "";
         req.on('data', function (chunks) {
@@ -47,7 +58,7 @@ routes.addRoute('/insert', function (req, res) {
 
             connection.query("insert into mahasiswa set ?", data_post, function (err, field) {
                 if (err) {
-                    throw err;
+                    if (err) throw err;
                 }
                 res.writeHead(302, {"Location": "/"});
                 res.end();
@@ -63,9 +74,9 @@ routes.addRoute('/insert', function (req, res) {
     }
 });
 
-routes.addRoute('/update/:id', function (req, res) {
+router.get('/update/:id', function (req, res) {
     connection.query("select * from mahasiswa where ?", {
-        no_induk: this.params.id
+        no_induk: req.params.id
     }, function (err, rows, fields) {
         if (rows.length) {
             var data = rows[0];
@@ -87,9 +98,9 @@ routes.addRoute('/update/:id', function (req, res) {
                         }
                     ], function (err, field) {
                         if (err) {
-                            throw err;
+                            if (err) throw err;
                         }
-                        res.writeHead(302, {"Location": "/"});
+                        res.writeHead(302, {"Location": app_url});
                         res.end();
                         // console.log(field.affectedRows);
                         // res.end();
@@ -97,7 +108,8 @@ routes.addRoute('/update/:id', function (req, res) {
                 })
             } else {
                 var html = view.compileFile('./template/form_update.html')({
-                    data: data
+                    data: data,
+                    app_url: app_url
                 });
 
                 res.writeHead(200, {"Content-Type": "text/html"});
@@ -112,12 +124,12 @@ routes.addRoute('/update/:id', function (req, res) {
     });
 });
 
-routes.addRoute('/delete/:id', function (req, res) {
+router.get('/delete/:id', function (req, res) {
     connection.query("delete from mahasiswa where ?", {
         no_induk: this.params.id,
     }, function (err, field) {
         if (err) {
-            throw err;
+            if (err) throw err;
         }
         // rows.forEach(function (item) {
         //     console.log(item.no_induk + " ==> " + item.nama);
@@ -127,18 +139,9 @@ routes.addRoute('/delete/:id', function (req, res) {
     });
 });
 
-http.createServer(function (req, res) {
-    var path = url.parse(req.url).pathname;
-    var match = routes.match(path);
-    if (match) {
-        match.fn(req, res); // request, response
-    } else {
-        var html = view.compileFile('./template/404.html')({
-            title: "Page Not Found"
-        });
-        res.writeHead(404, {"Content-Type": "text/html"});
-        res.end(html);
-    }
-}).listen(8888);
+app.use(app_url, router);
 
-console.log('Server is running');
+const app_port = '3000';
+app.listen(process.env.PORT || app_port, function () {
+    console.log(`Server is running on port: ${process.env.PORT || app_port}`);
+});
